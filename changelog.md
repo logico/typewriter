456cec4316ad2572adab18c06b810bcdd92c6071
logico <aamaral@logico.com.ar>
2018-01-30 12:14:18 -0300

    Vim airline theme

--- changelog.md
+++ changelog.md
@@ -2,6 +2,7 @@
 
 ## [0.0.1] - 2018-01-30
 - Created the installer
+- Added typewriter vim airline theme
 
 ## [0.0.1] - 2018-01-30
 - Project initialization
--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 ln -s /home/logico/dotfiles/rices/typewriter.vim /home/logico/.vim/colors/typewriter.vim
+ln -s /home/logico/dotfiles/rices/typewriter/typewriter-airline.vim /home/logico/.vim/bundle/vim-airline-themes/autoload/airline/themes/typewriter.vim
